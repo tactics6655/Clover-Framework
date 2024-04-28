652bcd319767116e601c995167800509c417fb5c
kdps <kdp7584@gmail.com>
2024-04-29 08:31:13 +0900

    pub

--- root/index.php
+++ root/index.php
@@ -2,6 +2,10 @@
 
 use Clover\Framework\Component\Runtime;
 
+if (!file_exists("./../vendor/autoload.php")) {
+    exit("Please run 'composer install' under application root directory");
+}
+
 include("./../vendor/autoload.php");
 
 define('__ROOT__', __DIR__);
